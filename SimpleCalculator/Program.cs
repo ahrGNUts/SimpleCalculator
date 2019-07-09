7d5689f2a9df99c9425bd458610013695a151858
Patrick <revmydev@gmail.com>
2019-07-09 14:59:28 -0700

    lay out main program logic

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,20 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            InputConverter inputConverter = new InputConverter();
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+            Console.WriteLine("Enter a number: ");
+            double firstNum = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            Console.WriteLine("Enter another number: ");
+            double secondNum = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            Console.WriteLine("Enter the operation to perform ( [add | +] | [subtract | -] | [ multiply | x | *] | [ divide | /] ): ");
+            string operation = Console.ReadLine();
+
+            double result = calculatorEngine.Calculate(operation, firstNum, secondNum);
+            Console.WriteLine(result);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
