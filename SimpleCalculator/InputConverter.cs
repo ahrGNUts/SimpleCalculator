f576b1747ca3ba6b9b9090cf9b3946702f2ad060
Patrick <revmydev@gmail.com>
2019-07-09 15:02:20 -0700

    InputConverter class

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class InputConverter
+{
+    public double ConvertInputToNumeric(string argTextInput)
+    {
+        double convertedNumber;
+
+        if (!Double.TryParse(argTextInput, out convertedNumber))
+            throw new ArgumentException("Expected a numeric value");
+
+        return convertedNumber;
+    }
+}
