0e7f48537413e2f57b33550e8050b43167e4466f
Patrick <revmydev@gmail.com>
2019-07-09 15:09:38 -0700

    added unit tests for calculator engine

---
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultFromNonSymbolOperation()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            double result = _calculatorEngine.Calculate("add", num1, num2);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultFromSymbolOperation()
+        {
+            int num1 = 4;
+            int num2 = 2;
+            double result = _calculatorEngine.Calculate("+", num1, num2);
+            Assert.AreEqual(6, result);
+        }
+    }
+}
---
+++ SimpleCalculator.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2fe91463-3661-48df-8899-c128b5597d1f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
