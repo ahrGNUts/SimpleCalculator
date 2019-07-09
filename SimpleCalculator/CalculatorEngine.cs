7a70cdf88ecafeafec21b66a74d660caa7329418
Patrick <revmydev@gmail.com>
2019-07-09 15:03:10 -0700

    CalculatorEngine logic

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class CalculatorEngine
+{
+    public double Calculate(string argOperation, double argNum1, double argNum2)
+    {
+        double result;
+
+        switch (argOperation.ToLower())
+        {
+            case "add":
+            case "+":
+                result = argNum1 + argNum2;
+                break;
+            case "subtract":
+            case "-":
+                result = argNum1 - argNum2;
+                break;
+            case "multiply":
+            case "*":
+            case "x":
+                result = argNum1 * argNum2;
+                break;
+            case "divide":
+            case "/":
+                result = argNum1 / argNum2;
+                break;
+            default:
+                throw new InvalidOperationException("Specified operation is not recognized");
+        }
+
+        return result;
+    }
+}
